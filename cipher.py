7cbc80658e0f1a7c1850fcf22f85b94eab692f5e
Chris Lambert <chrslmbrt@gmail.com>
2023-11-12 17:44:49 -0500

    second attempt

--- cipher.py
+++ cipher.py
@@ -1,46 +1,25 @@
 # add your code here
-#This assignment specifically mentions shift.  I need to figure this out.
+#encrypt only
+encrypt_key = {
+    'A': 'F', 'B': 'G', 'C': 'H', 'D': 'I', 'E': 'J', 'F': 'K',
+    'G': 'L', 'H': 'M', 'I': 'N', 'J': 'O', 'K': 'P', 'L': 'Q',
+    'M': 'R', 'N': 'S', 'O': 'T', 'P': 'U', 'Q': 'V', 'R': 'W',
+    'S': 'X', 'T': 'Y', 'U': 'Z', 'V': 'A', 'W': 'B', 'X': 'C',
+    'Y': 'D', 'Z': 'E',
+    'a': 'f', 'b': 'g', 'c': 'h', 'd': 'i', 'e': 'j', 'f': 'k',
+    'g': 'l', 'h': 'm', 'i': 'n', 'j': 'o', 'k': 'p', 'l': 'q',
+    'm': 'r', 'n': 's', 'o': 't', 'p': 'u', 'q': 'v', 'r': 'w',
+    's': 'x', 't': 'y', 'u': 'z', 'v': 'a', 'w': 'b', 'x': 'c',
+    'y': 'd', 'z': 'e'
+}
 
-def create_shift_dict(shift):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    return dict(zip(alphabet, shifted_alphabet)), dict(zip(shifted_alphabet, alphabet))
+decrypted_input = input("Please enter a message to encrypt: ")
+encrypted_output = ""
 
-encrypt_key, decrypt_key = create_shift_dict(5)
+for char in decrypted_input:
+    if char in encrypt_key:
+        encrypted_output += encrypt_key[char]
+    else:
+        encrypted_output += char
 
-ed = input("Would you like to encrypt or decrypt a message (not case sensitive)? ")
-
-if ed.lower() == "encrypt":
-    decrypted_input = input("Please enter a message to encrypt: ")
-    encrypted_output = ""
-
-    for char in decrypted_input:
-        lower_char = char.lower()
-        if lower_char in encrypt_key:
-            # Preserve the case of the original character
-            encrypted_char = encrypt_key[lower_char]
-            encrypted_output += encrypted_char.upper() if char.isupper() else encrypted_char
-        else:
-            encrypted_output += char
-
-    print("Your decrypted message: ", decrypted_input)
-    print("Your encrypted message: ", encrypted_output)
-
-elif ed.lower() == "decrypt":
-    encrypted_input = input("Please enter a message to decrypt: ")
-    decrypted_output = ""
-
-    for char in encrypted_input:
-        lower_char = char.lower()
-        if lower_char in decrypt_key:
-            # Preserve the case of the original character
-            decrypted_char = decrypt_key[lower_char]
-            decrypted_output += decrypted_char.upper() if char.isupper() else decrypted_char
-        else:
-            decrypted_output += char
-    
-    print("Your encrypted message: ", encrypted_input)
-    print("Your decrypted message: ", decrypted_output)
-
-else:
-    print("Invalid option chosen. Please type 'encrypt' or 'decrypt'. Input is forced into lower case and is not case sensitive.")
+print("Your encrypted output: ", encrypted_output)
